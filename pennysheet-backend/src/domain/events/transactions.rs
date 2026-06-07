303e7f366d6cb38c147bf8eebc4127370c15a574
Tri D Luu <91616223+triluu03@users.noreply.github.com>
2026-06-07 21:06:41 +0300

    UUIDv5

--- pennysheet-backend/src/domain/events/transactions.rs
+++ pennysheet-backend/src/domain/events/transactions.rs
@@ -5,29 +5,35 @@ use serde::{
     Deserialize,
     Serialize,
 };
+use uuid::Uuid;
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct ImportTransactionsRequested {
-    request_id: String,
+    request_id: Uuid,
     start_date: Date,
     end_date: Date,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct ImportTransactionsCompleted {
-    request_id: String,
+    request_id: Uuid,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct ImportTransactionsFailed {
-    request_id: String,
+    request_id: Uuid,
 }
 
 impl ImportTransactionsRequested {
     /// Import transactions requested constructor.
     pub fn new(start_date: Date, end_date: Date) -> Self {
+        let uuid_key = "ImportTransactionsRequested:".to_string()
+            + &start_date.to_string()
+            + ":"
+            + &end_date.to_string();
+
         ImportTransactionsRequested {
-            request_id: "1".to_string(),
+            request_id: Uuid::new_v5(&Uuid::NAMESPACE_OID, uuid_key.as_bytes()),
             start_date,
             end_date,
         }
