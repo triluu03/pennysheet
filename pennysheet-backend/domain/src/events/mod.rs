a7647cc3e4eec52cfa1c6d697bd6358881059ba2
Tri D Luu <91616223+triluu03@users.noreply.github.com>
2026-06-17 08:54:39 +0300

    New command and events for retry failed import

--- pennysheet-backend/domain/src/events/mod.rs
+++ pennysheet-backend/domain/src/events/mod.rs
@@ -14,10 +14,17 @@ use transactions::*;
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 #[cfg_attr(feature = "sea-orm-support", derive(FromJsonQueryResult))]
 pub enum Event {
-    // Transactions-related events.
+    /// A new transaction import is requested.
     ImportTransactionsRequested(ImportRequestData),
+    /// A pending import request has completed.
     ImportTransactionsCompleted(ImportStatusData),
+    /// A pending import request has failed.
     ImportTransactionsFailed(ImportStatusData),
+    /// A pending import request continues with a continuation key.
     ImportTransactionsContinued(ImportContinueData),
+    /// A retry for a failed transaction import is requested.
+    // TODO: use a better name for the data part instead of `ImportStatusData`?
+    TransactionImportRetryRequested(ImportStatusData),
+    /// A transaction is recorded.
     TransactionRecorded(TransactionData),
 }
