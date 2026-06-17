817676969686488b7c9c75369a7f72d7f97f20dc
Tri D Luu <91616223+triluu03@users.noreply.github.com>
2026-06-17 13:05:39 +0300

    Disable INFO logs from sqlx

--- pennysheet-backend/src/telemetry.rs
+++ pennysheet-backend/src/telemetry.rs
@@ -13,7 +13,12 @@ use tracing_subscriber::{
 pub fn init_tracing() -> Result<(), String> {
     let filter = EnvFilter::from_default_env()
         //.add_directive(Level::DEBUG.into())
-        .add_directive(Level::INFO.into());
+        .add_directive(Level::INFO.into())
+        .add_directive(
+            "sqlx=warn"
+                .parse()
+                .map_err(|error| format!("failed to parse sqlx tracing directive: {}", error))?,
+        );
 
     fmt()
         .with_env_filter(filter)
