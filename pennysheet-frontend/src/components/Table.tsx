5d7b8234693a137afdd7c2e5cdaed47694247443
Tri Luu <91616223+triluu03@users.noreply.github.com>
2026-07-25 16:50:37 +0300

    gha: this is the one? (#43)

--- pennysheet-frontend/src/components/Table.tsx
+++ pennysheet-frontend/src/components/Table.tsx
@@ -13,7 +13,7 @@ export interface EditableColumn<K extends string> {
 
 interface TableProps<K extends string> {
   // TODO: have a better type interface for the data.
-  data: Record<string, unknown>[];
+  data: Record<string, any>[];
   rowIdColumn: string;
   tableColumns: TableColumn<K>[];
   editableColumns: EditableColumn<K>[];
